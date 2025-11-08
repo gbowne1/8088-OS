eeb61d1cf6deb900f38a44965036bd47b37ce9b1
Gregory Bowne <gbowne144@gmail.com>
2025-11-08 05:23:09 -0800

    Update kernel.asm
    Add comment blocks. add timer IVT and IRQ handles

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -7,6 +7,10 @@ start:
     mov es, ax
     mov ss, ax
     mov sp, 0xFFFE
+
+    call install_timer_handler
+    call enable_irq0
+
     sti
 
     mov si, msg
@@ -15,6 +19,47 @@ start:
 hang:
     jmp hang
 
+; -------------------------------
+; Timer Interrupt Handler (INT 08h)
+; -------------------------------
+timer_handler:
+    push ax
+    push dx
+
+    mov dx, 0x3F8         ; COM1 port (optional debug output)
+    mov al, '.'           ; Send a dot to serial port
+    out dx, al
+
+    pop dx
+    pop ax
+    iret
+
+; -------------------------------
+; Install Timer Handler into IVT
+; -------------------------------
+install_timer_handler:
+    cli
+    mov ax, cs
+    mov ds, ax
+    mov es, ax
+
+    mov word [0x0020], timer_handler   ; Offset for INT 08h
+    mov word [0x0022], cs              ; Segment for INT 08h
+    sti
+    ret
+
+; -------------------------------
+; Enable IRQ0 in PIC
+; -------------------------------
+enable_irq0:
+    in al, 0x21           ; Read PIC mask
+    and al, 0xFE          ; Clear bit 0 (IRQ0)
+    out 0x21, al          ; Write back
+    ret
+
+; -------------------------------
+; Print String Routine
+; -------------------------------
 print_string:
     mov ah, 0x0E
 .next_char:
